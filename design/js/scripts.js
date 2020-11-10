16bcfdaf349e910dbd2227d293468794be0e390d
César Mejía <cesar.mejia@bancoazteca.com>
2020-11-10 09:56:15 -0600

    Create loader styles and behavior

--- design/js/scripts.js
+++ design/js/scripts.js
@@ -1,5 +1,23 @@
 (function() {
     'use strict';
-
     console.log('Scripts loaded.');
+
+    /**
+     * Remove loader from DOM.
+     */
+    function removeLoader() {
+        var loader = document.querySelector('.loader');
+        
+        if (loader) {
+            loader.classList.remove('loader-visible');
+            loader.addEventListener('transitionend', function() {
+                loader.parentElement.removeChild(loader);
+            });
+        }
+    }
+    
+    window.addEventListener('load', function() {
+        removeLoader();
+    });
+
 })();
